d06ec43fc52eb63e7d239a6e7d4f8e1be4cfabfe
Marcelo Crisostomo <marcelocrisostomodeveloper@gmail.com>
2023-07-18 23:59:25 -0400

    update

--- ejerciciom2.js
+++ ejerciciom2.js
@@ -1,11 +1,18 @@
-let nombre = "Marcelo Crisóstomo";
+let nombre = "Marcelo Crisostomo";
 let edad = 36;
 var eresUnDesarrollador = true;
-let fechaNacimiento = new date (1987, 3, 9);
+let fechaNacimiento = new Date (1987, 03, 09);
 const libro = {
     titulo: "Eloquent JavaScript",
     autor: "Marijn Haverbeke",
     fechaPublicacion: new Date("2018-11-04"),
     url: "https://eloquentjs-es.thedojo.mx/"
 }
-console.log("Hola mi nombre es ".nombre+" mi edad es".edad +" años")
+console.log("Hola mi nombre es " + nombre +" mi edad es de " +edad +" años")
+if (eresUnDesarrollador=true) {
+    console.log("Soy desarrollador Front - End ");
+}
+else{
+    console.log("No soy un desarrollador");
+}
+console.log(libro);
