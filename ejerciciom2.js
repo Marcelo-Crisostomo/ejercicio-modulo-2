304cb17aab7a2bbc527ccfba01e99b83c0d1f491
Marcelo Crisostomo <marcelocrisostomodeveloper@gmail.com>
2023-07-18 23:57:15 -0400

    hola

---
+++ ejerciciom2.js
@@ -0,0 +1,11 @@
+let nombre = "Marcelo Crisóstomo";
+let edad = 36;
+var eresUnDesarrollador = true;
+let fechaNacimiento = new date (1987, 3, 9);
+const libro = {
+    titulo: "Eloquent JavaScript",
+    autor: "Marijn Haverbeke",
+    fechaPublicacion: new Date("2018-11-04"),
+    url: "https://eloquentjs-es.thedojo.mx/"
+}
+console.log("Hola mi nombre es ".nombre+" mi edad es".edad +" años")
